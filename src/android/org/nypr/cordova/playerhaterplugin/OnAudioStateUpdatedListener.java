e0564c70b437348d848f468e70f7a906456a62c0
Brad Kammin <bkammin@nypublicradio.org>
2015-09-08 11:35:24 -0400

    added libvlc-3.0.0 + supporting files

--- src/android/org/nypr/cordova/playerhaterplugin/OnAudioStateUpdatedListener.java
+++
@@ -1,8 +0,0 @@
-package org.nypr.cordova.playerhaterplugin;
-
-
-public interface OnAudioStateUpdatedListener {
-	public abstract void onAudioStateUpdated(BasicAudioPlayer.STATE state);
-	public abstract void onAudioProgressUpdated(int progress, int duration);
-	public abstract void onAudioStreamingError(int reason);
-}
